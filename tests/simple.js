988a6f3ab3b6f7ddc5668f5f427637d6cc776dea
Markus Felten <Markus.Felten@gmx.de>
2011-11-30 21:24:45 +0100

    severity added

--- tests/simple.js
+++ tests/simple.js
@@ -4,10 +4,13 @@ var vows = require('vows'),
 
 vows.describe('Attribute').addBatch({
     'Attribute Values': {
-        topic:  new notification.notification({ "id" : "de.mfelten.test.1" }),
+        topic:  new notification.notification({ "id" : "de.mfelten.test.1", "severity" : notification.severity.SUCCESS }),
 
         'id is present': function (topic) {
             assert.equal (topic.id, "de.mfelten.test.1");
+        },
+        'severity is present': function (topic) {
+            assert.equal (topic.severity, notification.severity.SUCCESS);
         }
     }
 }).export(module);
