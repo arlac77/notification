26f78916e53edee2adf9eea7a06a46bb20fe7333
Markus Felten <Markus.Felten@gmx.de>
2011-12-01 15:07:40 +0100

    added action attribute

--- tests/simple.js
+++ tests/simple.js
@@ -4,13 +4,18 @@ var vows = require('vows'),
 
 vows.describe('Attribute').addBatch({
     'Attribute Values': {
-        topic:  new notification.notification({ "id" : "de.mfelten.test.1", "severity" : notification.severity.SUCCESS }),
+        topic:  new notification.notification({ "id" : "de.mfelten.test.1",
+        "severity" : notification.severity.SUCCESS,
+        "action" : notification.action.IGNORE }),
 
         'id is present': function (topic) {
             assert.equal (topic.id, "de.mfelten.test.1");
         },
         'severity is present': function (topic) {
             assert.equal (topic.severity, notification.severity.SUCCESS);
+        },
+        'action is present': function (topic) {
+            assert.equal (topic.action, notification.action.IGNORE);
         }
     }
 }).export(module);
