fa5d6b757a0b9826683ca67e80c35360f7b68b87
Markus Felten <Markus.Felten@gmx.de>
2011-12-04 16:50:48 +0100

    split into definition + incarnation

--- tests/simple.js
+++ tests/simple.js
@@ -2,11 +2,15 @@ var vows = require('vows'),
     assert = require('assert'),
     notification  = require('../lib/notification');
 
-vows.describe('Attribute').addBatch({
-    'Attribute Values': {
-        topic:  new notification.notification({ "id" : "de.mfelten.test.1",
-        "severity" : notification.severity.SUCCESS,
-        "action" : notification.action.IGNORE }),
+vows.describe('Notification Definition').addBatch({
+    'Definition Attributes': {
+        topic:  new notification.definition(
+            { "id" : "de.mfelten.test.1",
+              "severity" : notification.severity.SUCCESS,
+              "action" : notification.action.IGNORE,
+              "title" : "a simple notification",
+              "description" : "some more text",
+              "hint" : "ignore anything here" }),
 
         'id is present': function (topic) {
             assert.equal (topic.id, "de.mfelten.test.1");
@@ -16,6 +20,9 @@ vows.describe('Attribute').addBatch({
         },
         'action is present': function (topic) {
             assert.equal (topic.action, notification.action.IGNORE);
+        },
+        'title is present': function (topic) {
+            assert.equal (topic.title, "a simple notification");
         }
     }
 }).export(module);
